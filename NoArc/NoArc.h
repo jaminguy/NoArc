62617ba44707d0352d2127f7ae0eec5cc4a91718
Jamin Guy <jaminguy@gmail.com>
2011-10-29 10:52:15 -0500

    added example no ARC class

---
+++ NoArc/NoArc.h
@@ -0,0 +1,15 @@
+//
+//  NoArc.h
+//  NoArc
+//
+//  Created by Jamin Guy on 10/29/11.
+//  Copyright (c) 2011 Griffin Technology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NoArc : NSObject {
+    NSArray *someStuff;
+}
+
+@end
---
+++ NoArc/NoArc.m
@@ -0,0 +1,22 @@
+//
+//  NoArc.m
+//  NoArc
+//
+//  Created by Jamin Guy on 10/29/11.
+//  Copyright (c) 2011 Griffin Technology. All rights reserved.
+//
+
+#import "NoArc.h"
+
+@implementation NoArc
+
+- (id)init {
+    self = [super init];
+    if(self) {
+        someStuff = [[NSArray array] retain];
+    }
+    return self;
+}
+
+
+@end
