125502412fa4d3f9a9b6835e2ac7ecf58146e9fa
Jamin Guy <jaminguy@gmail.com>
2011-10-29 10:25:17 -0500

    Initial Commit

---
+++ NoArc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NoArc
+//
+//  Created by Jamin Guy on 10/29/11.
+//  Copyright (c) 2011 Griffin Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NoArc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NoArc
+//
+//  Created by Jamin Guy on 10/29/11.
+//  Copyright (c) 2011 Griffin Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
